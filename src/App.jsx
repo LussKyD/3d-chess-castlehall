d13ba8334ddba7073a64a2ccf396a4d226e2f498
KiDD LUSSKY <86703873+LussKyD@users.noreply.github.com>
2025-10-08 10:58:41 +0300

    Update v0 App.jsx
    chore: use CastleHall scene as root in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import CastleHallScene from './scenes/CastleHallScene'
+import CastleHall from './components/CastleHall'
 
-export default function App(){
+export default function App() {
   return (
-    <div className="app">
-      <Canvas shadows camera={{ position: [0, 12, 18], fov: 50 }}>
-        <ambientLight intensity={0.6} />
-        <directionalLight intensity={0.8} position={[10, 20, 10]} castShadow />
-        <CastleHallScene />
-        <OrbitControls target={[0,1,0]} />
-      </Canvas>
-
-      <div className="ui-overlay">
-        <button id="new-game">New Game</button>
-      </div>
+    <div style={{ width: '100vw', height: '100vh', background: '#100a05' }}>
+      <CastleHall />
     </div>
   )
 }
